aade726b4ec17a4f7ad055dac2b53f5c451c503b
napoléon <eliassaint.matthias@gmail.com>
2026-01-30 10:47:55 +0100

    m

---
+++ dependance/ecs.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+
+
+
+
+namespace ecs{
+class entity{
+    public:
+
+
+    private:
+
+};
+
+class component{
+    public:
+    void create();
+
+    private:
+
+
+
+};
+
+};
